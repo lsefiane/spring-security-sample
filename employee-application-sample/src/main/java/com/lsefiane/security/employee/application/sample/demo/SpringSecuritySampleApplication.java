10219bf173da3cc8eee9d570699e79ef10ce5a7e
lsefiane <lyes.sefiane@gmail.com>
2021-02-27 16:22:56 -0500

    ~ Project Structure Refactoring.

---
+++ employee-application-sample/src/main/java/com/lsefiane/security/employee/application/sample/demo/SpringSecuritySampleApplication.java
@@ -0,0 +1,13 @@
+package com.lsefiane.security.employee.application.sample.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecuritySampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecuritySampleApplication.class, args);
+	}
+
+}
---
+++ employee-application-sample/src/test/java/com/lsefiane/security/employee/application/sample/demo/SpringSecuritySampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lsefiane.security.employee.application.sample.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecuritySampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
